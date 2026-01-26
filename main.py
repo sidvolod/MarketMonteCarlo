ca1c68be728941eff6635b3faa61f287c86c4462
Volodymyr Sydorenko <sidvolod@gmail.com>
2026-01-26 16:39:33 +0000

    Added docstrings and configured code to follow PEP-8

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ import requests
 import models
 import json
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 
-def get_market_data (ticker):
+def get_market_data (ticker : models.Ticker):
+    """
+    Fetches market data for given ticker from Finage API
 
+    Args:
+        ticker (Ticker): Instance of the Ticker class
+    Returns:
+        (Json-Object): JSON data from the API
+        None if failed to fetch data
+    """
 
     API_KEY = os.getenv("FINAGE_API_KEY")
+
     if not API_KEY:
         print("couldnt fetch API key, check .env file")
         return
@@ -32,6 +42,31 @@ def get_market_data (ticker):
     except requests.exceptions.HTTPError as e:
         print(f"HTTP error: {e.response.status_code} for {url}")
     return None
+
+def process_data_to_dataframe(data):
+    """
+    Parses raw JSON from Finage API into a cleaned Pandas DataFrame
+
+    Args:
+        data (JSON-Object): JSON object with market data
+    Returns:
+        pd.DataFrame: cleaned OHLCV data indexed by datetime
+        None if the results is missing in the JSON
+    """
+
+    if not "results" in data:
+        print("No data found in the response")
+        return None
+
+    df = pd.DataFrame(data["results"])
+    df = df.rename(columns={"o": "Open", "h": "High", "l": "Low",
+                            "c": "Close", "v": "Volume"})
+
+    df["Date"] = pd.to_datetime(df["t"], unit="ms")
+    df.set_index("Date", inplace=True)
+
+    return df[["Open", "High", "Low", "Close", "Volume"]]
+
 if __name__ == "__main__":
     dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
     load_dotenv(dotenv_path)
@@ -41,8 +76,3 @@ if __name__ == "__main__":
     # data = get_market_data(ticker)
     with open("data/market_data.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-    # data_frame = pd.DataFrame(data["results"])
-    # data_frame = data_frame.rename(columns={"o": "open", "h": "high", "l": "low", "c": "close", "v": "volume", "t": "time"})
-    # data_frame["time"] = pd.to_datetime(data_frame["time"], unit="ms")
-    # data_frame.set_index("time", inplace=True)
-    # print(data_frame.to_string(index=True))
